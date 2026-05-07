3cf84b6535007f7eda67d5a7de13684a66a2a63f
wtshex1 <wtshexx@protonmail.com>
2026-05-07 02:18:54 +0000

    Remove Capacitor dependencies

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.crytotool.vault',
-  appName: 'CrytoTool Vault',
-  webDir: 'dist'
-};
-
-export default config;
