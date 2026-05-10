fdc9a05174bac163a8ca53a8aa7e6baac25a75c9
wtshex1 <wtshexx@protonmail.com>
2026-05-10 21:11:32 +0300

    Delete src/types/heroicons.d.ts

--- src/types/heroicons.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '@heroicons/react/24/solid' {
-  import { ComponentType, SVGProps } from 'react';
-  export type IconProps = SVGProps<SVGSVGElement>;
-  export type IconComponent = ComponentType<IconProps>;
-  const icons: Record<string, IconComponent>;
-  export default icons;
-}
